b5c030a0810da316b18c21d16c54dc0cc2a4189c
Hieu Nguyen <hieu.nv2001hust@gmail.com>
2024-10-15 22:44:50 +0700

    11

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import ServiceDetail1 from "./components/services/ServiceDetail1";
 import ServiceDetail2 from "./components/services/ServiceDetail2";
 import ServiceDetail3 from "./components/services/ServiceDetail3";
@@ -9,10 +9,7 @@ import { Main } from "./Main";
 
 function App() {
   const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-  const [theme, setTheme] = useLocalStorage(
-    "theme",
-    defaultDark ? "dark" : "light"
-  );
+  const [theme, setTheme] = useLocalStorage("theme", defaultDark ? "dark" : "light");
 
   const switchTheme = () => {
     const newTheme = theme === "light" ? "dark" : "light";
@@ -20,17 +17,15 @@ function App() {
   };
 
   return (
-    <Router>
-      <div className="app" data-theme={theme}>
-        <Routes basename={process.env.PUBLIC_URL}>
-          <Route path="/" element={<Main theme={theme} switchTheme={switchTheme} />} /> {/* Truyền props vào Main */}
-          <Route path="/service-detail-1" element={<ServiceDetail1 />} />
-          <Route path="/service-detail-2" element={<ServiceDetail2 />} />
-          <Route path="/service-detail-3" element={<ServiceDetail3 />} />
-          <Route path="/service-detail-4" element={<ServiceDetail4 />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="app" data-theme={theme}>
+      <Routes>
+        <Route path="/" element={<Main theme={theme} switchTheme={switchTheme} />} />
+        <Route path="/service-detail-1" element={<ServiceDetail1 />} />
+        <Route path="/service-detail-2" element={<ServiceDetail2 />} />
+        <Route path="/service-detail-3" element={<ServiceDetail3 />} />
+        <Route path="/service-detail-4" element={<ServiceDetail4 />} />
+      </Routes>
+    </div>
   );
 }
 
